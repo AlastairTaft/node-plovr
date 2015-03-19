6254b5525426abe85ac0a9677a89ec8912b699ab
Alastair Taft <ali.c.taft@gmail.com>
2015-03-20 09:48:07 +1100

    It works.
    Added the plovr.jar

--- plovr.js
+++ plovr.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
 
-process.stdout.write("TEST" + '\n');
+var spawn = require('child_process').spawn,
+    child;
+
+// java -jar ' + __dirname + '/plovr/plovr.jar build ' + plovrFilePath
+var args = process.argv.slice(2);
+args.splice(0, 0, '-jar', __dirname + '/plovr.jar');
+
+console.log(args);
+
+var proc = spawn('java', args, { stdio: 'inherit' });
+proc.on('exit', function (code, signal) {
+  process.on('exit', function(){
+    if (signal) {
+      process.kill(process.pid, signal);
+    } else {
+      process.exit(code);
+    }
+  });
+});
+
+// terminate children.
+process.on('SIGINT', function () {
+  proc.kill('SIGINT'); // calls runner.abort()
+  proc.kill('SIGTERM'); // if that didn't work, we're probably in an infinite loop, so make it die.
+  process.kill(process.pid, 'SIGINT');
+});
